25ae5be67fdd4c67a37195b59bdb9495043d1dc0
kesha7788 <keshark7788@gmail.com>
2025-06-02 22:53:47 +0400

    Creation4

--- maining/maining/links.h
+++ maining/maining/links.h
@@ -4,11 +4,19 @@
 
 #include <windows.h>
 #include <sstream>
+#include <cstdlib>
+#include <cmath>
+#include <limits>
+#include <ctime>
+#include <algorithm>
 
 #include "UIControllerHEAD.h"
 #include "InputController.h"
 #include "OutputController.h"
 
+
+#include "Discriminant.h"
 #include "Calculator.h"
+#include "Secret.h"
 
 using namespace std;
