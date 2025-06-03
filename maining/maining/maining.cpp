edbcea6e986654c501410784070c276274236290
kesha7788 <keshark7788@gmail.com>
2025-06-03 21:46:10 +0400

    Creation4

--- maining/maining/maining.cpp
+++ maining/maining/maining.cpp
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 ﻿#include <iostream>
 #include "UIControllerHEAD.h"
 #include "InputController.h"
+=======
+﻿#include "links.h"
+
+>>>>>>> parent of 25ae5be (Creation4)
 int main()
 {
     /*string name = "HUH";
@@ -11,6 +16,7 @@ int main()
     uicontroler.PrintMas(s);*/
 
     InputController inputcontroller;
+<<<<<<< HEAD
     string k;
     string d = inputcontroller.SaveInput(k, "Name: ");
 
@@ -18,4 +24,45 @@ int main()
     int f = inputcontroller.ChooseInput(list);
     cout << d;
 
+=======
+    inputcontroller.*/
+    setlocale(0, "rus");
+    string Enter, help;
+    help = "1.Help - Shows functions\n\
+2.Break - Turns off the program \n\
+3.Clear - Clear console \n\
+4.Calculator - Simple calculator\n";
+    cout << "Available functions: " << endl << help << endl;
+    //example: else if (Enter == "" || Enter == "") {}
+    while (Enter != "break" || Enter != "Break") {
+        cout << "Enter: ";
+        cin >> Enter;
+        cout << endl;
+        if (Enter == "Help" || Enter == "help" || Enter == "HELP" || Enter == "1") { cout << "Help: " << endl << help << endl; }
+        else if (Enter == "Clear" || Enter == "clear" || Enter == "CLEAR" || Enter == "2") { system("cls"); }
+        else if (Enter == "exit" || Enter == "Exit" || Enter == "EXIT") { break; }
+        else if (Enter == "Calculator" || Enter == "calculator" || Enter == "4") {
+            system("cls");
+            cout << "Calculator:" << endl;
+            cout << "if you want to exit calculator mode then write: break" << endl;
+            Calculator calc;
+            string line;
+            while (true) {
+                cout << "Enter an example: ";
+                getline(cin, line);
+                if (line == "exit" || line == "Exit" || line == "EXIT") { break; }
+                double num1, num2, result;
+                char operation;
+                istringstream ss(line);
+                if (ss >> num1 >> operation >> num2) {
+                    result = calc.calculate(num1, operation, num2);
+                    cout << num1 << " " << operation << " " << num2 << " = " << result << endl << endl;
+                }
+            }
+        } 
+
+        else { cout << "This function is not yet available!" << endl << "Try the available features: " << endl << help << endl; }
+    };
+    return 0;
+>>>>>>> parent of 25ae5be (Creation4)
 }
